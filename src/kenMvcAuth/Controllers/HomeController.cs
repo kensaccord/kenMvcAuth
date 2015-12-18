a6a57a9a89fd83eb719cff45916e3c835a950b55
Kendrick Thomas <Ken.L.Thomas@gmail.com>
2015-12-18 16:09:55 -0600

    Update to wedding Site

--- src/kenMvcAuth/Controllers/HomeController.cs
+++ src/kenMvcAuth/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using kenMvcAuth.Models.Helpers;
 using Microsoft.AspNet.Mvc;
 
 namespace kenMvcAuth.Controllers
@@ -10,25 +11,56 @@ namespace kenMvcAuth.Controllers
     {
         public IActionResult Index()
         {
+            HomeHelper hm = new HomeHelper();
+            
+            ViewData["CountDown"] = hm.GetCountDown(); ;
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "RSVP is comming soon";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Mailing Address.";
 
             return View();
         }
 
         public IActionResult Error()
         {
+            return View();
+        }
+		
+        public IActionResult OurStory()
+        {
+            ViewData["Message"] = "These are our stories";
+
+            return View();
+        }
+
+        public IActionResult Accommodations()
+        {
+            ViewData["Message"] = "Here are the local accommodations";
+
+            return View();
+        }
+
+        public IActionResult Registry()
+        {
+            ViewData["Message"] = "These are the places we are registered";
+
+            return View();
+        }
+
+        public IActionResult Events()
+        {
+            ViewData["Message"] = "List of wedding events";
+
             return View();
         }
     }
